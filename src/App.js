ddf68a207e5b81730ba76771aa296d6f81a33480
swidtech <swidtech1@gmail.com>
2024-08-06 23:10:42 +0100

    styling the form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { TodoWrapper } from './component/TodoWrapper';
 
 function App() {
   return (
     <div className="App">
-      <TodoWrapper />
+     <todoWrapper />
     </div>
   );
 }
--- src/component/TodoForm.js
+++ src/component/TodoForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export const Todo = () => {
+export const TodoForm = () => {
     return (
-        <div>Todo</div>
+        <div>TodoForm</div>
     )
 }
